71ed06a4f1aea3a42b4c5df65a044a37555a343e
Andre Pires <erdnapfaserip@gmail.com>
2025-09-17 16:29:02 -0400

    Tabela team povoada corretamente

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,39 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+echo $($PSQL "TRUNCATE TABLE games, teams RESTART IDENTITY")
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+
+do
+  if [[ $YEAR != year ]]
+  then
+    #get winner team_id
+    TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+
+    #if not found
+    if [[ -z $TEAM_ID ]]
+    then
+      #insert name
+      INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      if [[ $INSERTED_WINNER_RESULT == "INSERT 0 1" ]]
+      then
+        echo INSERTED TEAM:$WINNER
+      fi
+    fi
+
+    #get opponent team_id
+    TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
+    #if not found
+    if [[ -z $TEAM_ID ]]
+    then
+      #insert name
+      INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      if [[ $INSERT_OPPONENT_RESULT == "INSERT 0 1" ]]
+      then
+        echo INSERTED TEAM:$OPPONENT
+      fi
+    fi
+  fi
+done
